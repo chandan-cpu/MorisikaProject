14d7cc86853d9a993433df20ed821b77d3159a0b
chandan-cpu <chandanbo345@gmail.com>
2026-04-04 11:10:58 +0530

    email send error

--- backend/config/sendEmail.js
+++ backend/config/sendEmail.js
@@ -5,7 +5,9 @@ const sendEmail = async (email, subject, text) => {
     try {
         console.log("Sending email to:", email);
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true, // VERY IMPORTANT
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
