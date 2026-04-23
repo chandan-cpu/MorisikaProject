dfc87d46049f3a5e9e8c3e6f54dc3f3310163fa5
chandan-cpu <chandanbo345@gmail.com>
2026-04-23 10:52:40 +0530

    404 page error solve

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import store from './redux/store.js'
@@ -8,9 +7,9 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')).render(
 
-    <BrowserRouter>
+    <HashRouter>
        <Provider store={store}>
          <App />
        </Provider>
-    </BrowserRouter>
+    </HashRouter>
 )
